401ca2ff7684896725e2c02c7770c28b2d2d6ae4
vitaliy.chernysh <vivich4omega@gmail.com>
2023-06-23 16:04:35 +0200

    Exception handler added

--- src/main/java/com/example/demo/controller/adviser/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/adviser/GlobalExceptionHandler.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
-public class GlobalHandler {
+public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
-    public void NotSuchElementHandler(){}
+    public void NotSuchElementExceptionHandler(){}
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler({IllegalArgumentException.class, PropertyValueException.class})
-    public void IllegalArgumentHandler(){}
+    public void IllegalArgumentExceptionHandler(){}
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(SecurityException.class)
+    public void SecurityException(){}
 }
